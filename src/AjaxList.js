fad7797a63e89cdd96ae07ec4299cd9d91e6915d
m-szalik <szalik@jsoftware.org>
2016-12-06 03:11:22 +0100

    List, AjaxList refactoring

--- src/AjaxList.js
+++ src/AjaxList.js
@@ -1,21 +1,22 @@
 import React, { PropTypes } from 'react';
-import List from './List';
+import List, {SimpleListTable, ListPagination} from './List';
 import {shallowCopy} from './utils';
 import {_buildElement, devOnly} from './utils-internal';
 
 
 
 function copyProps(props) {
-    return shallowCopy({}, props, ['fetchDataCallback', 'onDataChanged', 'onFetch', 'onSuccess', 'onError', 'renderRow', 'showPagination', 'loadingComponent', 'errorComponent']);
+    return shallowCopy({}, props, ['fetchDataCallback', 'onFetch', 'onSuccess', 'onError', 'renderRow', 'showPagination', 'loadingComponent', 'errorComponent']);
 }
 
-export default class AjaxList extends List {
+export default class AjaxList extends React.Component {
+
     static propTypes = {
+        id : React.PropTypes.string, // list id
         fetchDataCallback : React.PropTypes.func.isRequired, // func that returns ajax promise.
-        onFetch : React.PropTypes.func, // when data loading starts
-        onError : React.PropTypes.func, // when data loading finishes with error
-        onSuccess : React.PropTypes.func, // when data loading finishes with success
-        onDataChanged : React.PropTypes.func, // inherited from List
+        onFetch : React.PropTypes.func, // when data loading starts, function(event)
+        onError : React.PropTypes.func, // when data loading finishes with error, function(event)
+        onSuccess : React.PropTypes.func, // when data loading finishes with success, function(event)
         errorComponent : React.PropTypes.oneOfType([ React.PropTypes.func, React.PropTypes.element ]),          // component function or element
         loadingComponent : React.PropTypes.oneOfType([ React.PropTypes.func, React.PropTypes.element ]),        // component function or element
         renderRow : React.PropTypes.func.isRequired,
@@ -23,6 +24,7 @@ export default class AjaxList extends List {
     };
 
     static defaultProps = {
+        id : "ajaxList-" + (Math.random() * 10000),
         showPagination : true,
         className : "ajaxList row",
         errorComponent : (<div className="center-block ajaxList-error">An error occurred.</div>),
@@ -32,6 +34,7 @@ export default class AjaxList extends List {
     constructor(props) {
         super(props);
         this.props = props;
+        this.id = props.id || 'ajaxList-' + (Math.random() * 10000);
         this._fetchData = this._fetchData.bind(this);
         this._handlePageChange = this._handlePageChange.bind(this);
         this.updateAndResetPage = this.updateAndResetPage.bind(this);
@@ -40,10 +43,10 @@ export default class AjaxList extends List {
         this.state = { items : null, error : null };
         this.htmlProps = copyProps(props);
         this.loadingElement = _buildElement(props.loadingComponent, {}, []);
+        this.state = {"items":[],"paging":{"total":0,"page":1,"count":1}};
     }
 
     componentWillReceiveProps(newProps) {
-        super.componentWillReceiveProps(newProps);
         if (this.props.loadingComponent != newProps.loadingComponent) {
             this.loadingElement = _buildElement(newProps.loadingComponent, {}, []);
         }
@@ -71,9 +74,13 @@ export default class AjaxList extends List {
         if (promise) {
             promise.then((resp) => {
                 if (Array.isArray(resp.data)) {
-                    this.pagesCount = page + 1;
+                    let page = this.state.paging.page +1;
+                    const np = {... this.state.paging};
+                    np.page = page;
+                    this.setState({ items : resp.data, paging : np });
+                } else {
+                    this.setState(resp.data);
                 }
-                this.data(resp.data);
                 if (this.props.onSuccess) {
                     this.props.onSuccess({ page : page, data : resp.data });
                 }
@@ -83,11 +90,11 @@ export default class AjaxList extends List {
                 if (this.props.onError) {
                     this.props.onError(err);
                 }
-                const errorElement = _buildElement(props.loadingComponent, {error:err}, []);
+                const errorElement = _buildElement(this.props.errorComponent, {error:err}, []);
                 this.setState({error: errorElement});
             });
         } else {
-            this.data({"items":[],"paging":{"total":0,"page":1,"count":1}}); // empty list
+            this.setState({"items":[],"paging":{"total":0,"page":1,"count":1}}); // empty list
         }
     }
 
@@ -96,7 +103,6 @@ export default class AjaxList extends List {
     }
 
     _handlePageChange(pg) {
-        super._handlePageChange(pg);
         this._fetchData(pg, true);
     }
 
@@ -109,14 +115,28 @@ export default class AjaxList extends List {
     }
 
     render() {
-        if (this.state.items == null) {
-            if (this.state.error == null) {
-                return (<div {...this.htmlProps}>{this.loadingElement}</div>);
+        const {paging, items, error} = this.state;
+        if (items == null) {
+            if (error == null) {
+                return (<div id={this.id} {...this.htmlProps}>{this.loadingElement}</div>);
             } else {
-                return (<div {...this.htmlProps}>{this.state.error}</div>);
+                return (<div id={this.id} {...this.htmlProps}>{error}</div>);
             }
         } else {
-            return super.render();
+            return (
+                <div id={this.id} {...this.htmlProps}>
+                    <SimpleListTable id={this.id} renderRow={this.props.renderRow} data={items}>
+                        {this.props.children}
+                    </SimpleListTable>
+                    {(() => {
+                        if (this.props.showPagination) {
+                            return (<ListPagination onPageChanged={this._handlePageChange} id={this.id} total={paging.total} count={paging.count} page={paging.page}/>);
+                        } else {
+                            return null;
+                        }
+                    })()}
+                </div>
+            );
         }
     }
 }
--- src/List.js
+++ src/List.js
@@ -1,147 +1,100 @@
 import React, { PropTypes } from 'react';
+import {isEquivalent, shallowCopy} from './utils';
+import {devOnly} from './utils-internal';
 
-export default class List extends React.Component {
+export class ListPagination extends React.Component {
     static propTypes = {
-        data : React.PropTypes.any, // object with property items or an array
-        renderRow : React.PropTypes.func.isRequired,
-        onPageChanged : React.PropTypes.func,
-        onDataChanged : React.PropTypes.func,
-        showPagination : React.PropTypes.bool, // default false
-        pagesCount : React.PropTypes.number // number of pages (have to be set in data is an array and pagination is used)
-    };
-
-    static defaultProps = {
-        showPagination : false
+        onPageChanged: React.PropTypes.func.isRequired,
+        id : PropTypes.string.isRequired,
+        total : PropTypes.number.isRequired,
+        count : PropTypes.number.isRequired,
+        page : PropTypes.number.isRequired
     };
 
     constructor(props) {
         super();
-        this._inUse = false;
-        this._data = null;
-        this.thead = null;
-        this.tfoot = null;
-        this.state = { };
-        this.page = 0;
-        this.componentWillReceiveProps(props);
+        this.props = props;
         this._handlePageChange = this._handlePageChange.bind(this);
-        this._checkData = this._checkData.bind(this);
-        this.data = this.data.bind(this);
     }
 
-    componentWillReceiveProps(nextProps) {
-        this.pagesCount = nextProps.pagesCount;
-        this.props = nextProps;
-        let id = nextProps.id;
-        if (id == undefined) {
-            id = "list-" + (Math.random() * 10000);
-        }
-        this.id = id;
-        if (this.props.renderRow == undefined) {
-            throw 'Missing function renderRow(item,index,key):component';
-        }
-        if (nextProps.children != undefined) {
-            const children = Array.isArray(nextProps.children) ? nextProps.children : [nextProps.children];
-            for (let i = 0; i < children.length; i++) {
-                if (children[i] == null || children[i] == undefined) {
-                    continue;
-                }
-                switch (children[i].type) {
-                    case 'tfoot':
-                        this.tfoot = children[i];
-                        break;
-                    case 'thead':
-                        this.thead = children[i];
-                        break;
-                    default:
-                        console.error('List can contain thead or tfoot components only, but', children[i], 'found in list ' + id);
-                        throw new Error('List can contain thead or tfoot components only.');
-                }
-            }
-        }
-        if (nextProps.data) {
-            this.data(nextProps.data);
-        }
-    }
-
-    componentDidMount() {
-        if (this._data != null) {
-            this.setState(this._data);
-        }
-    }
-
-    componentWillUnmount() {
-        this._inUse = false;
-    }
-
-    componentWillMount() {
-        this._inUse = true;
-    }
-
-    _checkData(data) {
-        if (Array.isArray(data) && this.props.showPagination && this.pagesCount == undefined) {
-            throw new Error('Got array of data and pagination was required but pagesCount is not set.');
-        }
-    }
-
-    data(data) {
-        if (data == null || data == undefined) {
-            return this.state;
-        }
-        this._checkData(data);
-        let update;
-        if (Array.isArray(data)) {
-            update = {
-                items: data,
-                count: data.length,
-                total: data.length * this.pagesCount,
-                page:  this.page
-            };
-        } else {
-            update = {
-                items: data.items,
-                count: data.paging.count,
-                total: data.paging.total,
-                page:  data.paging.page
-            };
-        }
-        this._data = update;
-        if (this._inUse) {
-            this.setState(update);
-        }
-        if (this.props.onDataChanged != undefined) {
-            this.props.onDataChanged(update);
-        }
-    }
-
-    _handlePageChange(pg) {
-        if (this.props.onPageChanged != undefined) {
-            this.props.onPageChanged(pg);
-        }
-        this.page = pg;
-    }
-
-    _renderPart(component) {
-        if (component != undefined && component != null) {
-            return component;
-        } else {
-            return null;
-        }
+    _handlePageChange(page) {
+        this.props.onPageChanged(page);
     }
 
     render() {
         const self = this;
-        if (this.state.items == undefined) {
-            return null;
+        return (<div className="row pagination-container">
+            {(() => {
+                if (self.props.page != undefined) {
+                    let pages = Math.ceil(self.props.total / self.props.count);
+                    if (pages > 1) {
+                        return (
+                            <nav className="list-pagination" id={`${self.props.id}-pagination`}>
+                                <ul className="pagination">
+                                    {(() => {
+                                        let pg = [];
+                                        pg.push((<li key="pg-prev" className={self.props.page < 2 ? "disabled" : ""}><a onClick={function() { if (self.props.page > 1) { self._handlePageChange(self.props.page -1) }}} aria-label="Previous"><span>&laquo;</span></a></li>));
+                                        for (let p = 1; p <= pages; p++) {
+                                            pg.push((<li key={`pg-pg-${p}`} className={p == self.props.page ? "active" : ""}><a onClick={function() { if (p != self.props.page) { self._handlePageChange(p) }}}>{p}</a></li>));
+                                        }
+                                        pg.push((<li className={pages <= self.props.page ? "disabled" : ""} key="pg-next"><a onClick={function() { if (pages > self.props.page) { self._handlePageChange(self.props.page +1) }}}><span>&raquo;</span></a></li>));
+                                        return pg;
+                                    })()}
+                                </ul>
+                            </nav>
+                        );
+                    } else {
+                        return null;
+                    }
+                }
+            })()}
+        </div>);
+    }
+}
+export class SimpleListTable extends React.Component {
+    static propTypes = {
+        id: React.PropTypes.string.isRequired,
+        data: React.PropTypes.any, // object with property items or an array
+        renderRow: React.PropTypes.func.isRequired,
+        indexOffset : React.PropTypes.number
+    };
+
+    static defaultProps = {
+        indexOffset : 0
+    };
+
+    constructor(props) {
+        super();
+        this.state = {data : props.data};
+        this.id = props.id;
+        if (props.renderRow == undefined) {
+            throw 'Missing function renderRow(item,index,key):component';
         }
+        this.parts = SimpleListTable.parseListChildren(props.children);
+        this.props = props;
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props.children == undefined || nextProps.children != this.props.children) {
+            this.parts = SimpleListTable.parseListChildren(nextProps.children);
+        }
+        if(nextProps.data != this.props.data) {
+            this.setState({data : nextProps.data});
+        }
+    }
+
+    render() {
+        const {data} = this.state;
         return (
-            <div id={this.name} key={this.name}>
-                <table className={`table ${this.props.className}`} style={{width:'100%'}} id={this.name + "-table"}>
-                    {(() => { return this._renderPart(this.thead) })()}
-                    {(() => {
+            <table className={`table ${this.props.className}`} style={{width:'100%'}} id={this.id + "-table"}>
+                {this.parts.thead}
+                {(() => {
+                    if (data) {
                         const rows = [];
-                        this.state.items.forEach((item, index) => {
-                            let key = self.id + '-row-' + index;
-                            let newRow = self.props.renderRow(item, index, key);
+                        data.forEach((item, index) => {
+                            const realIndex = this.props.indexOffset + index;
+                            let key = this.id + '-row-' + realIndex;
+                            let newRow = this.props.renderRow(item, realIndex, key);
                             if (newRow != undefined && newRow != null) {
                                 if (Array.isArray(newRow)) {
                                     throw 'Can return single &lt;tr&gt; component only';
@@ -151,35 +104,120 @@ export default class List extends React.Component {
                             }
                         });
                         return (<tbody>{rows}</tbody>);
-                    })()}
-                    {(() => { return this._renderPart(this.tfoot) })()}
-                </table>
+                    } else {
+                        return null;
+                    }
+                })()}
+                {this.parts.tfoot}
+            </table>
+        );
+    }
+
+    static parseListChildren(childrenIn) {
+        const out = { tfoot: null, thead: null };
+        const children = Array.isArray(childrenIn) ? childrenIn : [childrenIn];
+        for (let i = 0; i < children.length; i++) {
+            if (children[i] == null || children[i] == undefined) {
+                continue;
+            }
+            switch (children[i].type) {
+                case 'tfoot':
+                    out.tfoot = children[i];
+                    break;
+                case 'thead':
+                    out.thead = children[i];
+                    break;
+                default:
+                    devOnly(() => { console.error('List can contain thead or tfoot components only, but', children[i], 'found in list'); });
+            }
+        }
+        return out;
+    }
+}
+
+function copyProps(props) {
+    return shallowCopy({}, props, ['data', 'count', 'renderRow', 'showPagination', 'onPageChanged', 'prepareDataForPage']);
+}
+
+
+export class List extends React.Component {
+    static propTypes = {
+        id : React.PropTypes.string, // list id
+        data : React.PropTypes.array, // data array (all available data)
+        renderRow : React.PropTypes.func.isRequired, // function(item,index,key) : tr element
+        count : React.PropTypes.number.isRequired, // number of items per page
+        onPageChanged : React.PropTypes.func,  // callback function(page) : void (page is 1-indexed)
+        prepareDataForPage : React.PropTypes.func, // function(data,page,count) : array of data (page is 1-indexed)
+        showPagination : React.PropTypes.bool // default false
+    };
+
+    static defaultProps = {
+        showPagination : true,
+        id : "list-" + (Math.random() * 10000),
+        prepareDataForPage : function(data,page,count) {
+            return data.slice((page -1)*count, page*count);
+        }
+    };
+
+    constructor(props) {
+        super();
+        this.state = { };
+        this.page = 1;
+        if (props.renderRow == undefined) {
+            throw new Error('Missing function renderRow(item,index,key):component');
+        }
+        this._handlePageChange = this._handlePageChange.bind(this);
+        this._data = this._data.bind(this);
+        this.props = props;
+        this.htmlProps = copyProps(props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const updateData = ! isEquivalent(this.props.data, nextProps.data);
+        this.props = nextProps;
+        this.htmlProps = copyProps(nextProps);
+        if (updateData) {
+            this._data(nextProps.data, true);
+        }
+    }
+
+    componentDidMount() {
+        this._data(this.props.data, true);
+    }
+
+    _data(data, resetPage) {
+        if (resetPage) {
+            this.page = 1;
+        }
+        if (data == null || data == undefined) {
+            data = [];
+        }
+        const update = {
+            items: this.props.prepareDataForPage(data, this.page, this.props.count),
+            total: data.length,
+            page:  this.page
+        };
+        this.setState(update);
+    }
+
+    _handlePageChange(pg) {
+        if (this.props.onPageChanged != undefined) {
+            this.props.onPageChanged(pg);
+        }
+        this.page = pg;
+        this._data(this.props.data, false);
+    }
+
+    render() {
+        const {items, total, page} = this.state;
+        return (
+            <div {...this.htmlProps} key={this.id}>
+                <SimpleListTable id={this.props.id} renderRow={this.props.renderRow} data={items} indexOffset={(page -1) * this.props.count}>
+                    {this.props.children}
+                </SimpleListTable>
                 {(() => {
-                    if (this.props.showPagination) {
-                        return (<div className="row pagination-container">
-                            {(() => {
-                                if (this.state.page != undefined) {
-                                    let pages = Math.ceil(self.state.total / self.state.count);
-                                    if (pages > 1) {
-                                        return (
-                                            <nav aria-label="Page navigation" className="list-pagination" id={`${this.id}-pagination`}>
-                                                <ul className="pagination">
-                                                    {(() => {
-                                                        let pg = [];
-                                                        pg.push((<li key="pg-prev" className={self.state.page < 2 ? "disabled" : ""}><a onClick={function() { if (self.state.page > 1) { self._handlePageChange(self.state.page -1) }}} aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>));
-                                                        for (let p = 1; p <= pages; p++) {
-                                                            pg.push((<li key={`pg-pg-${p}`} className={p == self.state.page ? "active" : ""}><a onClick={function() { if (p != self.state.page) { self._handlePageChange(p) }}}>{p}</a></li>));
-                                                        }
-                                                        pg.push((<li className={pages <= self.state.page ? "disabled" : ""} key="pg-next"><a onClick={function() { if (pages > self.state.page) { self._handlePageChange(self.state.page +1) }}} aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>));
-                                                        return pg;
-                                                    })()}
-                                                </ul>
-                                            </nav>
-                                        );
-                                    }
-                                }
-                            })()}
-                        </div>);
+                    if (items && this.props.showPagination) {
+                        return (<ListPagination onPageChanged={this._handlePageChange} id={this.props.id} total={total} count={this.props.count} page={page}/>);
                     } else {
                         return null;
                     }
