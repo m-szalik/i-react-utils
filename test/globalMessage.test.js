5a852d79a1db4eee28ee7a179bd504ae80394458
m-szalik <szalik@jsoftware.org>
2016-12-13 13:53:18 +0100

    GlobalMessage: tests

--- test/globalMessage.test.js
+++ test/globalMessage.test.js
@@ -10,3 +10,18 @@ test("Render: empty", () => {
     const gm = TestUtils.findRenderedDOMComponentWithClass(component, 'globalMessages');
     assert(gm.innerHTML == '', 'Are there some messages there?');
 });
+
+test("Render: message success & clear", () => {
+    const comp = TestUtils.renderIntoDocument(<GlobalMessage><span>Some text here</span></GlobalMessage>);
+
+    // test add
+    comp.message('success', 'A success message');
+    let domMessages = TestUtils.scryRenderedDOMComponentsWithClass(comp, 'alert');
+    assert(domMessages.length == 1, 'Invalid number of messages (should be one). => ' + domMessages.length);
+    assert(domMessages[0].innerHTML == 'A success message', 'Missing message');
+
+    // test clear
+    comp.clear();
+    domMessages = TestUtils.scryRenderedDOMComponentsWithClass(comp, 'alert');
+    assert(domMessages.length == 0, 'Invalid number of messages (should be zero). => ' + domMessages.length);
+});
