d0e83f78bb796d277dd3978fe80b1ddf81bce451
m-szalik <szalik@jsoftware.org>
2016-12-19 12:06:17 +0100

    examples: LazyLoad, AjaxList

--- example/app.js
+++ example/app.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import List from '../src/List';
+import AjaxList from '../src/AjaxList';
+import LazyLoad from '../src/LazyLoad';
 import * as fw from '../src/FormWizard';
 import GlobalMessage from '../src/GlobalMessage';
+import axios from 'axios';
 
+class LazyLoadContentComponent extends React.Component {
+    render() {
+        return (<div>
+            <code>{this.props.data.message}</code>
+            {this.props.children}
+        </div>);
+    }
+}
 
 class FormWizardInputWrapper extends React.Component {
     render() {
-        return (<div>
+        return (<div style={{border:"solid 1px gray", margin:"4px"}}>
                 <ul>
                     <li>Type: {this.props.type}</li>
                     <li>Label: {this.props.label}</li>
@@ -47,7 +58,9 @@ class ExamplePage extends React.Component {
 
     render() {
         return (<div className="container">
+            <a href="https://github.com/m-szalik/i-react-utils/blob/master/README.md">Project's home page on GitHub</a>
             <h1>Examples</h1>
+
             <div>
                 <h2>List</h2>
                 <button onClick={this.addPurpleHandler}>Add purple</button>
@@ -59,6 +72,42 @@ class ExamplePage extends React.Component {
                 </List>
             </div>
             <hr/>
+
+
+            <div>
+                <h2>AjaxList</h2>
+                <h3>Response format:</h3>
+                <pre>{JSON.stringify( {"items" : ["Item 20 (0)","Item 21 (1)"], "paging":{ "page":3, "count":10, "total":22 }} )}</pre>
+                <code>Param <strong>page</strong> starts from <strong>1</strong></code>
+                <h3>Result:</h3>
+                <AjaxList
+                    renderRow={function(item,index,reactRowKey) {
+                        return (<tr key={reactRowKey}><td>{index}</td><td>{JSON.stringify(item)}</td></tr>);
+                    }}
+                    fetchDataCallback={function(pageNum) {
+                        return axios.get('/api/ajax-list?page=' + pageNum);
+                    }}
+                >
+                    <thead><tr><th>index</th><th>item</th></tr></thead>
+                    <tfoot><tr><th colSpan="2">End of table</th></tr></tfoot>
+                </AjaxList>
+            </div>
+            <hr/>
+
+
+            <div>
+                <h2>LazyLoad</h2>
+                <LazyLoad component={LazyLoadContentComponent}
+                    ajax={function() {
+                        return axios.get('/api/lazy-load');
+                    }}
+                >
+                    <span>Cool :)</span>
+                </LazyLoad>
+            </div>
+            <hr/>
+
+
             <div>
                 <h2>FormWizard</h2>
                 <fw.Form onSubmit={this.submitCallback} wrapper={FormWizardInputWrapper} instantValidation={true}>
@@ -76,6 +125,7 @@ class ExamplePage extends React.Component {
                 </fw.Form>
             </div>
             <hr/>
+
         </div>);
     }
 }
--- example/server.js
+++ example/server.js
@@ -12,6 +12,22 @@ app.use(require('webpack-dev-middleware')(compiler, {
     publicPath: config.output.publicPath
 }));
 
+app.get('/api/ajax-list', function (req, res) {
+    const pg = parseInt(req.query.page || "1");
+    const items = [];
+    for(var j= 0,i=(pg-1) * 10; i<22 && j<10; i++, j++) {
+        items.push('Item ' + i + ' (' + j + ')');
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ "items":items,"paging":{"page":pg,"count":10,"total":22}  }));
+});
+
+app.get('/api/lazy-load', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ message : 'Message from server' }));
+});
+
+
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
